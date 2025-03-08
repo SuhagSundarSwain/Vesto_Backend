37b8152feb37f327927420560ea649c17f13b0fa
Suhag <suhagsundarswain1@gmail.com>
2025-03-08 20:59:54 +0530

    v1.0.0-Product entity and test demo controller added.

---
+++ src/main/java/com/example/demo/Entities/Product.java
@@ -0,0 +1,27 @@
+package com.example.demo.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private Double price;
+    private String category;
+    private Integer stock;
+    private String imageUrl;
+
+}

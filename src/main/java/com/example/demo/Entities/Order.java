4b8ae67a768b36e7cf51ee5a1daf8c41527fab22
Suhag <suhagsundarswain1@gmail.com>
2025-03-31 00:26:34 +0530

    v1.2.0-Order and OrderItems added

---
+++ src/main/java/com/example/demo/Entities/Order.java
@@ -0,0 +1,36 @@
+package com.example.demo.Entities;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    private double totalPrice;
+    private String status; // PENDING, CONFIRMED, DELIVERED, CANCELLED
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems;
+}
